d4ab584c86b18e2835b43120fd0ac1797ba5ea04
developerashikur1 <masik4506@gmail.com>
2023-09-02 03:20:55 +0600

    vercel added

--- dist/app/modules/books/books.constant.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.booksFilterableFields = exports.booksSearchableFields = void 0;
-exports.booksSearchableFields = ["title", "author", "genre"];
-exports.booksFilterableFields = ["searchTerm", "genre", "publicationYear"];
--- dist/constants/pagination.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.pagianationFileds = void 0;
-exports.pagianationFileds = ["page", "limit", "sortBy", "sortOrder"];
